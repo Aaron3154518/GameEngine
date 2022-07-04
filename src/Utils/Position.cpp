62ab54abd5525ae195d4578549070a7895cc81be
Aaron Orenstein <aaoren210@gmail.com>
2022-07-04 16:35:29 -0600

    Added templated get/set methods to rect

--- src/Utils/Position.cpp
+++ src/Utils/Position.cpp
@@ -1,28 +1,47 @@
 #include "Position.h"
 
+bool eq(double v1, double v2, double err) {
+    return std::abs(v1 - v2) <= err;
+}
+
+// Rect
 // Constructors
 Rect::Rect() : Rect(0, 0, 0, 0) {}
+Rect::Rect(const SDL_Rect& other) {
+    set<>(other);
+}
 
-template <>
-Rect::Rect<Rect::Align::TOP_LEFT>(double _x, double _y, double _w, double _h)
-    : x(_x), y(_y), w(_w), h(_h) {}
-
-template <>
-Rect::Rect<Rect::Align::BOT_RIGHT>(double _x2, double _y2, double _w, double _h)
-    : x(_x2 - _w), y(_y2 - _h), w(_w), h(_h) {}
-
-template <>
-Rect::Rect<Rect::Align::CENTER>(double _cx, double _cy, double _w, double _h)
-    : x(_cx - _w / 2.0), y(_cy - _h / 2.0), w(_w), h(_h) {}
-
-template <>
-Rect::Rect<Rect::Align::CORNERS>(double _x1, double _y1, double _x2, double _y2)
-    : x(_x1), y(_y1), w(_x2 - _x1), h(_y2 - _y1) {}
-
-Rect::Rect(const SDL_Rect& other)
-    : x(other.x), y(other.y), w(other.w), h(other.h) {}
+bool Rect::empty() const {
+    return eq(w, 0., ERR) && eq(h, 0., ERR);
+}
+bool Rect::invalid() const {
+    return w < 0 || h < 0;
+}
+void Rect::normalize() {
+    if (w < 0) {
+        x += w;
+        w = -w;
+    }
+    if (h < 0) {
+        y += h;
+        h = -h;
+    }
+}
 
 // Getters
+double Rect::x2() const {
+    return x + w;
+}
+double Rect::y2() const {
+    return y + h;
+}
+double Rect::cX() const {
+    return x + w / 2.0;
+}
+double Rect::cY() const {
+    return y + h / 2.0;
+}
+
 int Rect::X() const {
     return (int)(x + ERR);
 }
@@ -30,10 +49,10 @@ int Rect::Y() const {
     return (int)(y + ERR);
 }
 int Rect::X2() const {
-    return (int)(x + ERR + w + ERR);
+    return (int)(x2() + ERR);
 }
 int Rect::Y2() const {
-    return (int)(y + ERR + h + ERR);
+    return (int)(y2() + ERR);
 }
 int Rect::W() const {
     return X2() - X();
@@ -42,13 +61,99 @@ int Rect::H() const {
     return Y2() - Y();
 }
 int Rect::CX() const {
-    return (int)((w + ERR) / 2);
+    return (int)(cX() + ERR);
 }
 int Rect::CY() const {
-    return (int)((h + ERR) / 2);
+    return (int)(cY() + ERR);
+}
+
+// Setters
+// Position and dimensions
+template <>
+void Rect::setX<Rect::Align::TOP_LEFT>(double _x, double _w) {
+    x = _x;
+    w = _w;
+}
+template <>
+void Rect::setY<Rect::Align::TOP_LEFT>(double _y, double _h) {
+    y = _y;
+    h = _h;
+}
+
+template <>
+void Rect::setX<Rect::Align::BOT_RIGHT>(double _x2, double _w) {
+    x = _x2 - _w;
+    w = _w;
+}
+template <>
+void Rect::setY<Rect::Align::BOT_RIGHT>(double _y2, double _h) {
+    y = _y2 - _h;
+    h = _h;
+}
+
+template <>
+void Rect::setX<Rect::Align::CENTER>(double _cx, double _w) {
+    x = _cx - _w / 2.0;
+    w = _w;
+}
+template <>
+void Rect::setY<Rect::Align::CENTER>(double _cy, double _h) {
+    y = _cy - _h / 2.0;
+    h = _h;
+}
+
+template <>
+void Rect::setX<Rect::Align::CORNERS>(double _x1, double _x2) {
+    x = _x1;
+    w = _x2 - _x1;
+}
+template <>
+void Rect::setY<Rect::Align::CORNERS>(double _y1, double _y2) {
+    y = _y1;
+    h = _y2 - _y1;
+}
+
+// Position only
+template <>
+void Rect::setPosX<Rect::Align::TOP_LEFT>(double _x) {
+    x = _x;
+}
+template <>
+void Rect::setPosY<Rect::Align::TOP_LEFT>(double _y) {
+    y = _y;
+}
+
+template <>
+void Rect::setPosX<Rect::Align::BOT_RIGHT>(double _x2) {
+    x = _x2 - w;
+}
+template <>
+void Rect::setPosY<Rect::Align::BOT_RIGHT>(double _y2) {
+    y = _y2 - h;
+}
+
+template <>
+void Rect::setPosX<Rect::Align::CENTER>(double _cx) {
+    x = _cx - w / 2.0;
+}
+template <>
+void Rect::setPosY<Rect::Align::CENTER>(double _cy) {
+    y = _cy - h / 2.0;
+}
+
+template <>
+void Rect::setPosX<Rect::Align::CORNERS>(double _x) {
+    x = _x;
+}
+template <>
+void Rect::setPosY<Rect::Align::CORNERS>(double _y) {
+    y = _y;
 }
 
 // Operators
-Rect::operator SDL_Rect() {
+Rect::operator bool() const {
+    return !empty() && !invalid();
+}
+Rect::operator SDL_Rect() const {
     return SDL_Rect{X(), Y(), W(), H()};
 }
--- src/Utils/Position.h
+++ src/Utils/Position.h
@@ -3,6 +3,10 @@
 
 #include <SDL.h>
 
+#include <cmath>
+
+bool eq(double v1, double v2, double err);
+
 class Rect {
    public:
     enum Align : uint8_t {
@@ -16,10 +20,57 @@ class Rect {
     double ERR = 1e-5;
 
     Rect();
-    template <Align A = Align::TOP_LEFT>
-    Rect(double v1, double v2, double v3, double v4) {}
-    Rect(const SDL_Rect &other);
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    Rect(double v1x, double v1y, double v2x, double v2y) {
+        set<X>(v1x, v2x);
+        set<Y>(v1y, v2y);
+    }
+    Rect(const SDL_Rect& other);
     ~Rect() = default;
+
+    bool empty() const;
+    bool invalid() const;
+    void normalize();
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    void resize(double nW, double nH) {
+        double tmpX = getX<X>(), tmpY = getY<Y>();
+        w = nW;
+        h = nH;
+        setPos<X, Y>(tmpX, tmpY);
+    }
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    void resize(double factor) {
+        resize<X, Y>(w * factor, h * factor);
+    }
+
+    // Getters
+    double x2() const;
+    double y2() const;
+    double cX() const;
+    double cY() const;
+
+    template <Align A = Align::TOP_LEFT>
+    double getX() const {
+        return
+#if A == Align::BOT_RIGHT
+            x2();
+#elif A == Align::CENTER
+            cX();
+#else
+            x;
+#endif
+    }
+    template <Align A = Align::TOP_LEFT>
+    double getY() const {
+        return
+#if A == Align::BOT_RIGHT
+            y2();
+#elif A == Align::CENTER
+            cY();
+#else
+            y;
+#endif
+    }
 
     int X() const;
     int Y() const;
@@ -30,7 +81,57 @@ class Rect {
     int CX() const;
     int CY() const;
 
-    operator SDL_Rect();
+    // Setters
+    // Set positions and dimensions
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    void set(const Rect& r) {
+        setX<X>(r.getX<X>(),
+#if X == Align::CORNERS
+                r.x2()
+#else
+                r.w
+#endif
+        );
+
+        setY<Y>(r.getY<Y>(),
+#if Y == Align::CORNERS
+                r.y2()
+#else
+                r.h
+#endif
+        );
+    }
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    void set(double v1x, double v1y, double v2x, double v2y) {
+        setX<X>(v1x, v2x);
+        setY<Y>(v1y, v2y);
+    }
+
+    template <Align A = Align::TOP_LEFT>
+    void setX(double v1, double v2);
+    template <Align A = Align::TOP_LEFT>
+    void setY(double v1, double v2);
+
+    // Set only position
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    void setPos(const Rect& r) {
+        setPosX<X>(r.getX<X>());
+        setPosY<Y>(r.getY<Y>());
+    }
+    template <Align X = Align::TOP_LEFT, Align Y = X>
+    void setPos(double vx, double vy) {
+        setPosX<X>(vx);
+        setPosY<Y>(vy);
+    }
+
+    template <Align A = Align::TOP_LEFT>
+    void setPosX(double val);
+    template <Align A = Align::TOP_LEFT>
+    void setPosY(double val);
+
+    // Operators
+    operator bool() const;
+    operator SDL_Rect() const;
 
    private:
 };
