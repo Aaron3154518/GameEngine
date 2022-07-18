086bce290db143dea03af085aae2c68fce1e33e2
Aaron3154518 <aaoren210@gmail.com>
2022-07-17 18:54:22 -0600

    Renamed MouseService -> MouseServices

--- src/ServiceSystem/ServiceSystem.h
+++ src/ServiceSystem/ServiceSystem.h
@@ -16,8 +16,9 @@ class ServiceSystem {
    public:
     template <class T>
     static std::shared_ptr<T> Get() {
-        static_assert(std::is_base_of<ServiceBase, T>::value,
-                      "ServiceSystem::Get(): template type is not a subclass of Service");
+        static_assert(
+            std::is_base_of<ServiceBase, T>::value,
+            "ServiceSystem::Get(): template type is not a subclass of Service");
         auto it = Services().find(std::type_index(typeid(T)));
         if (it != Services().end()) {
             return std::static_pointer_cast<T>(it->second);
@@ -37,19 +38,23 @@ class ServiceSystem {
 
     template <class T>
     static std::shared_ptr<T> Add() {
-        static_assert(std::is_base_of<ServiceBase, T>::value,
-                      "ServiceSystem::Add(): template type is not a subclass of Service");
+        static_assert(
+            std::is_base_of<ServiceBase, T>::value,
+            "ServiceSystem::Add(): template type is not a subclass of Service");
         static_assert(!std::is_base_of<Component, T>::value,
-                      "ServiceSystem::Add(): Services cannot be Components, use Component-type member variables instead");
-        static_assert(std::is_constructible<T>::value,
-                      "ServiceSystem::Add(): the Service must be default constructible");
+                      "ServiceSystem::Add(): Services cannot be Components, "
+                      "use Component-derived member variables instead");
+        static_assert(
+            std::is_constructible<T>::value,
+            "ServiceSystem::Add(): the Service must be default constructible");
         std::shared_ptr<T> t = std::make_shared<T>();
         Services()[std::type_index(typeid(T))] = t;
         ((ServiceBase*)t.get())->init();
         return t;
     }
 
-    static std::unordered_map<std::type_index, std::shared_ptr<ServiceBase>>& Services();
+    static std::unordered_map<std::type_index, std::shared_ptr<ServiceBase>>&
+    Services();
 };
 
 #endif
