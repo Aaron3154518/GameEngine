dd639d69390ee771aaf4f57a3ce0b2f7310a9450
Aaron3154518 <aaoren210@gmail.com>
2022-07-17 18:57:42 -0600

    Moved ResizeService into EventServices

--- src/ServiceSystem/Includes.h
+++ src/ServiceSystem/Includes.h
@@ -4,10 +4,10 @@
 #include <ServiceSystem/Component.h>
 #include <ServiceSystem/CoreService/EventService.h>
 #include <ServiceSystem/CoreService/RenderService.h>
-#include <ServiceSystem/CoreService/ResizeService.h>
 #include <ServiceSystem/CoreService/UpdateService.h>
 #include <ServiceSystem/EventServices/DragService.h>
 #include <ServiceSystem/EventServices/MouseService.h>
+#include <ServiceSystem/EventServices/ResizeService.h>
 #include <ServiceSystem/Observable.h>
 #include <ServiceSystem/Service.h>
 #include <ServiceSystem/ServiceSystem.h>
